d1b17441611ffd13604fc7a1e1c3844a33166e62
MakaraChan <chanmakara333@gmail.com>
2018-02-03 00:02:44 +0700

    Home Commit

---
+++ app/src/main/java/makara/com/uberapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package makara.com.uberapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
